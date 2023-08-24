f157a53b011f906081f912415c5e850bc3e71556
baokyo002 <74838783+baokyo002@users.noreply.github.com>
2023-08-25 01:50:36 +0700

    Add movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moving : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.get("w")){
+            
+        }
     }
 }
